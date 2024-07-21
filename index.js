1557247a9e8f18ae4d50dcfa9e884d0fcf5ab0ad
madcampos <me@madcampos.dev>
2024-07-21 01:00:08 -0400

    feat: move state to html and implement accessible status
    Based on the logic provided by @evert for checking what to do next with each flask.
    The references were tweaked so the state is kept in the HTML.
    
    Added some better semantic markup to the content and specific ARIA roles so the game can be played with keyboard and a screen reader.

--- index.js
+++ index.js
@@ -19,7 +19,33 @@ const gameData = [
   [],
 ];
 
-let selectedFromFlask = 0;
+const flasksContainer = document.getElementById("flasks");
+const statusContainer = document.getElementById('status');
+
+/**
+ *
+ * @param {'selected' | 'deselected' | 'pour' | 'failedToPour' | 'reset'} status
+ * @param {number} [fromIndex]
+ * @param {number} [toIndex]
+ */
+function updateStatus(status, fromIndex, toIndex) {
+  switch (status) {
+    case "selected":
+      statusContainer.innerHTML = `Flask ${fromIndex + 1} selected.`;
+      break;
+    case "deselected":
+      statusContainer.innerHTML = `Flask ${fromIndex + 1} deselected.`;
+      break;
+    case "pour":
+      statusContainer.innerHTML = `Poured from Flask ${fromIndex + 1} to Flask ${toIndex + 1}.`;
+      break;
+    case "failedToPour":
+      statusContainer.innerHTML = `Cannot pour from Flask ${fromIndex + 1} to Flask ${toIndex + 1}, select another flask.`;
+      break;
+    default:
+      statusContainer.innerHTML = 'Select a flask.'
+  }
+}
 
 /**
  * @param {Flask} from
@@ -52,103 +78,121 @@ function isSameColor(from, color) {
   return from.at(-1) === color;
 }
 
-function print() {
-  //  outputEl.innerHTML = JSON.stringify(gameData, null, 2);
-}
-
-const outputEl = document.getElementById("output");
-const flasksEl = document.getElementById("flasks");
-
-print();
-
-document.querySelector("form")?.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  evt.stopPropagation();
-
-  const target = evt.target;
-  const formData = new FormData(/** @type {HTMLFormElement} */ (target));
-
-  const from = selectedFromFlask;
-  const to = Number.parseInt(/** @type {string} */ (formData.get("to")));
-
-  pour(gameData[from], gameData[to]);
-  setFlaskOpen(null);
-  print();
-  updateFlasks();
-});
-
-document.querySelector("form").addEventListener("input", (evt) => {
-  const target = /** @type {HTMLInputElement}*/ (evt.target);
-
-  if (target.matches('input[type="radio"]')) {
-    if (target.hasAttribute("aria-disabled")) {
-      evt.preventDefault();
-      evt.stopPropagation();
-    }
-
-    const inputSelector = `input[type="radio"][name="to"]`;
-
-    document
-      .querySelectorAll(inputSelector)
-      .forEach((/** @type {HTMLInputElement}*/ input) => {
-        input.toggleAttribute("aria-disabled", false);
-      });
-
-    /** @type {HTMLInputElement}*/ (
-      document.querySelector(`${inputSelector}[value="${target.value}"]`)
-    ).toggleAttribute("aria-disabled", true);
-  }
-});
-
 /**
- * @param {number} flaskIndex
- * @returns {HTMLDivElement}
+ * @param {HTMLElement} flaskElement
+ * @param {Flask} flask
  */
-function renderFlask(flaskDiv, flask, flaskIndex) {
-  flaskDiv.replaceChildren();
-  for (let i = FLASK_SIZE - 1; i >= 0; i--) {
-    const segment = document.createElement("div");
-    segment.classList.add("segment");
-    segment.style.backgroundColor = flask[i];
-    flaskDiv.appendChild(segment);
+function renderFlask(flaskElement, flask) {
+  let list = flaskElement.querySelector("ol");
+
+  if (!list) {
+    list = document.createElement("ol");
+
+    const description = document.createElement("span");
+
+    description.classList.add('visually-hidden');
+    description.innerHTML = `Flask ${Number.parseInt(flaskElement.dataset.index) + 1}, ${FLASK_SIZE} parts.`;
+
+    flaskElement.appendChild(description);
   }
 
-  return flaskDiv;
+  list.replaceChildren();
+
+  for (let i = FLASK_SIZE - 1; i >= 0; i--) {
+    const segment = document.createElement("li");
+
+    segment.classList.add("segment");
+    segment.dataset.color = flask[i];
+    segment.ariaLabel = flask[i] ?? 'No color';
+
+    list.appendChild(segment);
+  }
+
+  flaskElement.appendChild(list);
 }
 
 function updateFlasks() {
-  const flaskElems = Array.from(document.querySelectorAll(".flask"));
+  const flaskElements = /** @type {HTMLElement[]} */ (Array.from(document.querySelectorAll(".flask")));
+
   for (let i = 0; i < gameData.length; i++) {
-    renderFlask(flaskElems[i], gameData[i], i);
+    renderFlask(flaskElements[i], gameData[i]);
   }
 }
 
 /**
- * @param {number|null} flaskIndex
+ * @param {HTMLElement} flaskElement
  */
-function setFlaskOpen(flaskIndex) {
-  selectedFromFlask = flaskIndex;
-  document.querySelectorAll(".flask").forEach((flask, idx) => {
-    if (idx === flaskIndex) {
-      flask.classList.add("pouring");
-    } else {
-      flask.classList.remove("pouring");
-    }
+function setFlaskOpen(flaskElement) {
+  flaskElement.setAttribute("aria-pressed", "true");
+  updateStatus("selected", Number.parseInt(flaskElement.dataset.index));
+}
+
+/**
+ * @param {HTMLElement} flaskElement
+*/
+function setFlaskClosed(flaskElement) {
+  flaskElement.setAttribute("aria-pressed", "false");
+  updateStatus("deselected", Number.parseInt(flaskElement.dataset.index));
+}
+
+function setAllFlasksClosed() {
+  flasksContainer.querySelectorAll('.flask').forEach((flaskElement) => {
+    flaskElement.setAttribute("aria-pressed", "false");
   });
+}
+
+/**
+ * @param {HTMLElement} flaskElement
+ */
+function selectFlask(flaskElement) {
+  const fromFlaskElement = /** @type {HTMLElement} */(flasksContainer.querySelector('[aria-pressed="true"]'));
+
+  if (!fromFlaskElement) {
+    // No flask is selected
+    setFlaskOpen(flaskElement);
+  } else if (fromFlaskElement === flaskElement) {
+    // Close the flask
+    setFlaskClosed(flaskElement);
+  } else {
+    // A different flask is selected, atempt to pour!
+    const fromIndex = Number.parseInt(fromFlaskElement.dataset.index);
+    const toIndex = Number.parseInt(flaskElement.dataset.index);
+
+    if (canPour(gameData[fromIndex], gameData[toIndex])) {
+      updateStatus("pour", fromIndex, toIndex);
+    } else {
+      updateStatus("failedToPour", fromIndex, toIndex);
+    }
+
+    pour(gameData[fromIndex], gameData[toIndex]);
+    setAllFlasksClosed();
+    updateFlasks();
+  }
 }
 
 function initializeFlasks() {
   for (let i = 0; i < gameData.length; i++) {
-    const flaskDiv = document.createElement("div");
-    flaskDiv.classList.add("flask");
-    flaskDiv.addEventListener("click", () => {
-      setFlaskOpen(i);
-    });
-    renderFlask(flaskDiv, gameData[i], i);
+    const flaskElement = document.createElement("button");
+    flaskElement.classList.add("flask");
+    flaskElement.dataset.index = i.toString();
+    setFlaskClosed(flaskElement);
 
-    flasksEl.appendChild(flaskDiv);
+    renderFlask(flaskElement, gameData[i]);
+
+    flasksContainer.appendChild(flaskElement);
   }
+
+  flasksContainer.addEventListener("click", (evt) => {
+    const target = /** @type {HTMLElement} */(evt.target);
+
+    if (target.matches(".flask")) {
+      selectFlask(target);
+    }
+  });
+
+  updateStatus("reset");
 }
+
 initializeFlasks();
 
 function isGameOver() {
